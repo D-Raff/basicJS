3c05de2ba08708f47b828489fec52a0ff04a0c52
D-Raff <damonraffels@gmail.com>
2023-10-10 12:22:00 +0200

    added notes

--- script/code.js
+++ script/code.js
@@ -1,1 +1,66 @@
-console.log("My Name is, who? My Name is, what? My Name is, Damon")
+// let firstName = 'Damon'
+// console.log("My Name is, who? My Name is, what? My Name is "+ firstName)
+// console.log(firstName +" has no siblings")
+
+// console.log(`My Name is, who? My Name is, what? My Name is ${firstName}`)
+// console.log(`${firstName} has no siblings`)
+
+// let name1 = 'Damon'
+// console.log(`your name is ${name1}`)
+
+// const country = "South Africa" //const must have a variable (South Africa in this case), if it does not then it will return an error
+
+// primitive data types: string, number, bool or boolean, undefined, null
+// Dynamic data types: array (collection of data, use square brackets [] when creating arrays), object (use curly brackets {})
+
+
+// let person = {
+//     name:'Damon', //comma is necessary
+//     surname: 'Raffels',
+//     age:25
+// }
+// console.log(person) //this is an object
+
+
+// let data = 'Raffels' //use no quotation marks for numbers 
+// console.log(data);
+// console.log(typeof data); // identifying type of data
+// console.log(data[0]); //will show the first letter of a string
+
+// let numbers = [1,2,3,4,5]
+// console.log(numbers[3])
+// console.table(numbers) //displays arrays in a table
+// console.error("mistake");
+// console.warn("warning");
+
+// Operators
+/*
+-Arithmetic: +, -, /, *, **(exponent), %(modulus)
+*/
+// let numb1 = 2
+// let numb2 = 3
+// console.log(
+//     `sum of ${numb1} and ${numb2} is ${numb1 + numb2}`
+// );
+// console.log(
+//     `Difference of ${numb2} and ${numb1} is ${numb2 - numb1}`
+// );
+// console.log(
+//     `Product of ${numb1} and ${numb2} is ${numb1 * numb2}`
+// );
+// console.log(`${numb2}/${numb1} = ${numb2/numb1}`);
+// console.log(`${numb2}**2 = ${numb2 ** 2}`);
+// console.log(`what is the remainder of 12/2 = ${12%2}`);
+
+// -Assignment: =, +=,
+// let numb1 = "3"
+// let numb2 = 3
+// console.log(numb1 += numb2)
+
+//Comparison Result will always be a boolean (true or false)
+// console.log(numb1 > numb2);
+// console.log(numb1 < numb2);
+// console.log(numb1 != numb2);
+// console.log(numb1 == numb2); (double = will give true, but triple = will be false) (== will check only values but === will check data type too)
+
+// -Logical: ||, &&, !
